83ecddab350e74975c2e8ca6c531633323788143
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-09 20:13:34 +0530

    Maximux Number of three integer

--- src/test/java/com/maximumtest/MaximumTest.java
+++ src/test/java/com/maximumtest/MaximumTest.java
@@ -1,7 +1,30 @@
 package com.maximumtest;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MaximumTest {
-    public static void main(String[] args) {
-        System.out.println("Welcome");
+    private static  MaximumNumber maximumTest = new MaximumNumber();
+
+
+    @Test
+    public void whenGivenThreeNumbers_AndFirstNumberIsMax_ShouldReturn_true() {
+        Integer[] intArray = {3, 2, 1};
+        Integer result = maximumTest.findMaximum(intArray);
+        Assertions.assertEquals(Integer.valueOf(3), result);
+    }
+
+    @Test
+    public void whenGivenThreeNumbers_AndSecondNumberIsMax_ShouldReturn_true() {
+        Integer[] intArray = {3, 5, 1};
+        Integer result = maximumTest.findMaximum(intArray);
+        Assertions.assertEquals(Integer.valueOf(5), result);
+    }
+
+    @Test
+    public void whenGivenThreeNumbers_AndThirdNumberIsMax_ShouldReturn_true() {
+        Integer[] intArray = {3, 1, 7};
+        Integer result = maximumTest.findMaximum(intArray);
+        Assertions.assertEquals(Integer.valueOf(7), result);
     }
 }
